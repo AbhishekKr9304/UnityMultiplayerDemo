627dfd08d21984c5e5b4bf6c8e34d58edcfea3ff
UnityAKXR <kumarabhishek022412@gmail.com>
2026-04-26 23:21:49 +0530

    Network Authority - Lecture 11

---
+++ Assets/Script/ConnectionButtons.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ConnectionButtons : MonoBehaviour
+{
+    public void Join()
+    {
+        NetworkManager.Singleton.StartClient();
+        Debug.Log("Start Client");
+    }
+
+    public void Host()
+    {
+        NetworkManager.Singleton.StartHost();
+        Debug.Log("Start Host");
+    }
+}
